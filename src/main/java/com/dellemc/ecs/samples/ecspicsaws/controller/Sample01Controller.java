410ac348dd0d3ecbc3cf191f0c2f129623b83241
Jason Cwik <jason.cwik@emc.com>
2017-10-11 12:53:13 -0500

    Added user version of Sample01

--- src/main/java/com/dellemc/ecs/samples/ecspicsaws/controller/Sample01Controller.java
+++ src/main/java/com/dellemc/ecs/samples/ecspicsaws/controller/Sample01Controller.java
@@ -1,16 +1,13 @@
 package com.dellemc.ecs.samples.ecspicsaws.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.internal.Mimetypes;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.dellemc.ecs.samples.ecspicsaws.model.EcsConfiguration;
 import com.dellemc.ecs.samples.ecspicsaws.util.S3Factory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.context.embedded.MimeMappings;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.activation.MimeType;
-import javax.activation.MimetypesFileTypeMap;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -27,9 +22,9 @@ import java.io.IOException;
  * In this first sample, we will create a controller to upload a file to ECS.
  */
 @Controller
-public class Sample01ControllerSolution {
-    private static final String CONTROLLER = "/sample1sol";
-    private static final Log log = LogFactory.getLog(Sample01ControllerSolution.class);
+public class Sample01Controller {
+    private static final String CONTROLLER = "/sample1";
+    private static final Log log = LogFactory.getLog(Sample01Controller.class);
 
 
     /**
@@ -67,10 +62,9 @@ public class Sample01ControllerSolution {
         //    This is because the S3 PutObject API requires the size.  This is because unsized PUT requests tend to
         //    get truncated if you're not explicit about how many bytes are being sent and use chunked encoding.
         ObjectMetadata om = new ObjectMetadata();
-        om.setContentType(file.getContentType());
-        om.setContentLength(file.getSize());
+        // TODO: configure the ObjectMetadata
 
-        s3.putObject(config.getBucketName(), file.getOriginalFilename(), file.getInputStream(), om);
+        // TODO: Insert your code here to upload the object.
 
         String message = String.format("Successfully uploaded %s to bucket %s (%d bytes)",
                 file.getOriginalFilename(), config.getBucketName(), file.getSize());
